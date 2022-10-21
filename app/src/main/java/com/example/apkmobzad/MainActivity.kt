9d246efafb9e14967bfe78315e2c78dddb0546d7
PatrykD2pr <patrykdomaszek@gazeta.pl>
2022-10-21 15:09:12 +0200

    Zmiany w kodzie, skonczony program

--- app/src/main/java/com/example/apkmobzad/MainActivity.kt
+++ app/src/main/java/com/example/apkmobzad/MainActivity.kt
@@ -3,14 +3,21 @@ package com.example.apkmobzad
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var imien = findViewById<TextView>(R.id.textView)
+        var dodaj = findViewById<EditText>(R.id.editTextTextPersonName)
 
-
-
-
+        findViewById<Button>(R.id.button).setOnClickListener(){
+            imien.text = imien.text.toString() + dodaj.text
+        }
+        findViewById<Button>(R.id.button2).setOnClickListener(){
+            imien.text = "Mateusz Poklekowski"
+        }
     }
 }
