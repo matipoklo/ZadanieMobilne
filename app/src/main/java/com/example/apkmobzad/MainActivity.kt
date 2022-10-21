69c70d6e4197890449094744ec2b52c53fc14f0b
PatrykD2pr <patrykdomaszek@gazeta.pl>
2022-10-21 14:57:07 +0200

    Skonczony layout

--- app/src/main/java/com/example/apkmobzad/MainActivity.kt
+++ app/src/main/java/com/example/apkmobzad/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.apkmobzad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
     }
 }
